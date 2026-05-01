e181947d55d8ac0fb93e8891a9382e60e665a01c
seifblkhal-dev <seifblkhal@gmail.com>
2026-05-02 01:07:01 +0200

    script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const mediators = [
-    { name: "Abdallah Bendal", img: "image_224015.png", id: "", phone: "", fb: "#" },
-    { name: "Fahd L-y", img: "image_2cc343.png", id: "", phone: "0556397678", fb: "#" },
-    { name: "Seif Din Blkhal", img: "645973696_1463265602121726_7545117659013627702_n.jpg", id: "1232493977", phone: "", fb: "#" },
-    { name: "Omar Houni", img: "667807743_1988536058539268_8007520388332275956_n.jpg", id: "", phone: "", fb: "#" },
-    { name: "Ahmed Berk", img: "90ce1764-bb65-4ed8-a418-2d5b9961f2b3.jpg", id: "", phone: "0667038305", fb: "#" },
-    { name: "Aissa Ziat", img: "8baeb0fb-0319-49d6-86bf-ab9544ea5c54.jpg", id: "", phone: "0666545521", fb: "#" },
-    { name: "Lounes Chem's", img: "686509516_122161472120640020_7698140914024962050_n.jpg", id: "", phone: "", fb: "#" },
-    { name: "Abdel Wahab", img: "669885852_122132688795034284_3950083065487782380_n.jpg", id: "", phone: "", fb: "#" },
-    { name: "Benbadra Ahmed Ilyes", img: "682434788_1292941326314113_6149430041564653157_n.jpg", id: "", phone: "", fb: "#" },
-    { name: "Larbi Mohamed", img: "686093056_1678173696524194_2233143532774049149_n.jpg", id: "", phone: "0791521788", fb: "#" }
+    { name: "Abdallah Bendal", img: "abdallah.png", id: "", phone: "", fb: "#" },
+    { name: "Fahd L-y", img: "fahd.png", id: "", phone: "0556397678", fb: "#" },
+    { name: "Seif Din Blkhal", img: "seif.png", id: "1232493977", phone: "", fb: "#" },
+    { name: "Omar Houni", img: "omar.png", id: "", phone: "", fb: "#" },
+    { name: "Ahmed Berk", img: "berk.png", id: "", phone: "0667038305", fb: "#" },
+    { name: "Aissa Ziat", img: "aissa.png", id: "", phone: "0666545521", fb: "#" },
+    { name: "Lounes Chem's", img: "lounes.png", id: "", phone: "", fb: "#" },
+    { name: "Abdel Wahab", img: "wahab.png", id: "", phone: "", fb: "#" },
+    { name: "Benbadra Ahmed Ilyes", img: "badra.png", id: "", phone: "", fb: "#" },
+    { name: "Larbi Mohamed", img: "larbi.png", id: "", phone: "0791521788", fb: "#" }
 ];
 
 function render() {
@@ -16,21 +16,23 @@ function render() {
     if(!grid) return;
     grid.innerHTML = mediators.map(m => `
         <div class="card">
-            <img src="${m.img}" alt="${m.name}" loading="lazy">
+            <img src="${m.img}" alt="${m.name}" loading="lazy" onerror="this.src='logo.png'">
             <h3>${m.name}</h3>
             ${m.phone ? `<p><i class="fas fa-phone"></i> ${m.phone}</p>` : ''}
             ${m.id ? `
                 <div class="id-box">
                     <span>ID: <strong>${m.id}</strong></span>
-                    <i class="fas fa-copy copy-icon" onclick="copyId('${m.id}')"></i>
+                    <i class="fas fa-copy copy-icon" onclick="copyId('${m.id}')" title="نسخ"></i>
                 </div>` : ''}
-            <a href="${m.fb}" class="fb-link" target="_blank">Facebook</a>
+            <a href="${m.fb}" class="fb-link" target="_blank"><i class="fab fa-facebook-f"></i> Facebook</a>
         </div>
     `).join('');
 }
 
 function copyId(id) {
-    navigator.clipboard.writeText(id).then(() => alert("تم نسخ ID: " + id));
+    navigator.clipboard.writeText(id).then(() => {
+        alert("تم نسخ المعرف بنجاح: " + id);
+    });
 }
 
 window.onload = render;
