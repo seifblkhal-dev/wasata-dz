fef1b035508a384367a2bb223488ed7d5b6e68ac
seifblkhal-dev <seifblkhal@gmail.com>
2026-05-02 12:08:43 +0200

    script.js

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 const intermediaries = [
-    { id: 1, name: "Abdallah Bendal", fb: "https://www.facebook.com/abdallah.dz.104", phone: "", img: "https://via.placeholder.com/100", rating: 5 },
-    { id: 2, name: "Fahd L-y", fb: "https://www.facebook.com/profile.php?id=61556093836113", phone: "0556397678", img: "https://via.placeholder.com/100", rating: 5 },
-    { id: 3, name: "Seif Din Blkhal", fb: "https://www.facebook.com/seif.blkhal", phone: "ID: 1232493977", img: "https://via.placeholder.com/100", rating: 5 },
-    { id: 4, name: "Omar Houni", fb: "https://www.facebook.com/omar.houni.1", phone: "", img: "https://via.placeholder.com/100", rating: 5 },
-    { id: 5, name: "Ahmed Berk", fb: "https://www.facebook.com/ahmed.berk.7", phone: "0667038305", img: "https://via.placeholder.com/100", rating: 5 },
-    { id: 6, name: "Aissa Ziat", fb: "https://www.facebook.com/aissa.ziat", phone: "0666545521", img: "https://via.placeholder.com/100", rating: 5 },
-    { id: 7, name: "Lounes Chem's", fb: "https://www.facebook.com/lounes.chems", phone: "", img: "https://via.placeholder.com/100", rating: 5 },
-    { id: 8, name: "Abdel Wahab", fb: "https://www.facebook.com/abdel.wahab.dz", phone: "", img: "https://via.placeholder.com/100", rating: 5 },
-    { id: 9, name: "Benbadra Ahmed Ilyes", fb: "https://www.facebook.com/ilyes.benbadra", phone: "", img: "https://via.placeholder.com/100", rating: 5 },
-    { id: 10, name: "Larbi Mohamed", fb: "https://www.facebook.com/larbi.mohamed.984", phone: "0791521788", img: "https://via.placeholder.com/100", rating: 5 }
+    { id: 1, name: "Abdallah Bendal", fb: "https://www.facebook.com/abdallah.dz.104", phone: "0550000000", img: "https://seifblkhal-dev.github.io/wasata-dz/images/abdallah.jpg", rating: 5 },
+    { id: 2, name: "Fahd L-y", fb: "https://www.facebook.com/profile.php?id=61556093836113", phone: "0556397678", img: "https://seifblkhal-dev.github.io/wasata-dz/images/fahd.jpg", rating: 5 },
+    { id: 3, name: "Seif Din Blkhal", fb: "https://www.facebook.com/seif.blkhal", phone: "ID: 1232493977", img: "https://seifblkhal-dev.github.io/wasata-dz/images/seif.jpg", rating: 5 },
+    { id: 4, name: "Omar Houni", fb: "https://www.facebook.com/omar.houni.1", phone: "0660000000", img: "https://seifblkhal-dev.github.io/wasata-dz/images/omar.jpg", rating: 5 },
+    { id: 5, name: "Ahmed Berk", fb: "https://www.facebook.com/ahmed.berk.7", phone: "0667038305", img: "https://seifblkhal-dev.github.io/wasata-dz/images/ahmed.jpg", rating: 5 },
+    { id: 6, name: "Aissa Ziat", fb: "https://www.facebook.com/aissa.ziat", phone: "0666545521", img: "https://seifblkhal-dev.github.io/wasata-dz/images/aissa.jpg", rating: 5 },
+    { id: 7, name: "Lounes Chem's", fb: "https://www.facebook.com/lounes.chems", phone: "0770000000", img: "https://seifblkhal-dev.github.io/wasata-dz/images/lounes.jpg", rating: 5 },
+    { id: 8, name: "Abdel Wahab", fb: "https://www.facebook.com/abdel.wahab.dz", phone: "0550000000", img: "https://seifblkhal-dev.github.io/wasata-dz/images/wahab.jpg", rating: 5 },
+    { id: 9, name: "Benbadra Ahmed Ilyes", fb: "https://www.facebook.com/ilyes.benbadra", phone: "0660000000", img: "https://seifblkhal-dev.github.io/wasata-dz/images/ilyes.jpg", rating: 5 },
+    { id: 10, name: "Larbi Mohamed", fb: "https://www.facebook.com/larbi.mohamed.984", phone: "0791521788", img: "https://seifblkhal-dev.github.io/wasata-dz/images/larbi.jpg", rating: 5 }
 ];
 
 const container = document.getElementById('wasataContainer');
 
 function displayWasata() {
+    if(!container) return; // حماية في حال عدم وجود الحاوية
     container.innerHTML = '';
     
     intermediaries.forEach(person => {
@@ -26,17 +27,11 @@ function displayWasata() {
         }
 
         card.innerHTML = `
-            <div class="profile-img">
-                <img src="${person.img}" alt="${person.name}">
-            </div>
+            <img src="${person.img}" alt="${person.name}" class="user-img">
             <h3 class="name">${person.name}</h3>
             <p class="phone">${person.phone}</p>
-            <div class="rating-system">
-                ${starsHtml}
-            </div>
-            <a href="${person.fb}" target="_blank" rel="noopener noreferrer" class="fb-button">
-                Facebook <i class="fab fa-facebook"></i>
-            </a>
+            <div class="rating-system">${starsHtml}</div>
+            <a href="${person.fb}" target="_blank" rel="noopener noreferrer" class="fb-btn">Facebook</a>
         `;
         container.appendChild(card);
     });
@@ -50,4 +45,4 @@ function updateRating(id, newRating) {
     }
 }
 
-displayWasata();
+document.addEventListener('DOMContentLoaded', displayWasata);
